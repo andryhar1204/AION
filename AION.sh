55a52596a28b654d02c06983759f2804f94184c1
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-09-06 17:24:01 +0700

    Update AION.sh

--- AION.sh
+++ AION.sh
@@ -2,7 +2,7 @@
 
 POOL=cluster.aionpool.tech:3333
 
-WALLET=AION.0xa0934f2d6542e54262b02eb3b587b03e8b27b0861550bfefd704ddb5774dd9f4
+WALLET=AION:0xa0934f2d6542e54262b02eb3b587b03e8b27b0861550bfefd704ddb5774dd9f4
 
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-JOS
 cd "$(dirname "$0")"
