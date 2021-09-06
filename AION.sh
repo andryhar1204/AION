28c195c7bdbaa1302d29a0083d850c4a34db6a46
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-09-06 17:10:15 +0700

    Update AION.sh

--- AION.sh
+++ AION.sh
@@ -7,4 +7,4 @@ WALLET=AION.0xa0934f2d6542e54262b02eb3b587b03e8b27b0861550bfefd704ddb5774dd9f4
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-JOS
 cd "$(dirname "$0")"
 
-chmod +x ./bionic && sudo ./bionic --algo Equihash 210/9 --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
+chmod +x ./bionic && sudo ./bionic --algo Equihash --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
