43874d29bf11c5ff782193b6fd0f95846083ffa3
andryhar1204 <75998721+andryhar1204@users.noreply.github.com>
2021-09-06 17:03:42 +0700

    Create AION.sh

---
+++ AION.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+POOL=cluster.aionpool.tech:3333
+
+WALLET=AION.0xa0934f2d6542e54262b02eb3b587b03e8b27b0861550bfefd704ddb5774dd9f4
+
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-JOS
+cd "$(dirname "$0")"
+
+chmod +x ./bionic && sudo ./bionic --algo AION --pool $POOL --user $WALLET.$WORKER $@ --ethstratum ETHPROXY
